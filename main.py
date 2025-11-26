5fe6889d2a72bcb7d4a2b8ecad7a256452af8850
departamentoIA <caarteaga@asf.gob.mx>
2025-11-26 16:25:01 -0600

    main.py almost ready

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Last Updated:   2025-11-26
 Version:        1.0
 Description:
 Connection to a SQLServer DB for 5 tables and multiple values in column 'ReceptorRFC'.
-The query result for every table is saved in an excel file.
+The query result for every table is saved in an excel file (or multiple excel files).
 Dependencies:   pyodbc==5.3.0, pandas==2.3.3, openpyxl==3.1.5.
 Usage:          Every sql query has the form:
 SELECT TOP 4 *
@@ -34,7 +34,7 @@ table_list = ["2024-AECF_0101_Anexo4-Detalle-Percepciones"]
 receptor_RFC_list = ["PEES540914FT3"]
 # Create placeholders: "?, ?"
 placeholders = ", ".join("?" * len(receptor_RFC_list))
-MAX_ROWS_PER_TABLE = 600_000
+MAX_ROWS_PER_TABLE = 6
 
 # Connection configuration
 connection_string = (
@@ -46,7 +46,7 @@ connection_string = (
     "Encrypt=no;"              # o "Encrypt=yes;TrustServerCertificate=yes;"
 )
 
-
+# Construct every query for every table
 queries = []
 for table in table_list:
     query = f"""
@@ -57,41 +57,38 @@ for table in table_list:
     queries.append(query)
 
 
-def split_DataFrame(df: pd.DataFrame, max_rows: int) -> None:
-    """S"""
+def split_DataFrame(table: str, df: pd.DataFrame, max_rows=600000) -> None:
+    """Split every DataFrame and save it in different excel files,
+    if it has more that 600,000 rows."""
     total_rows = len(df)
-
     if total_rows == 0:
         print("El DataFrame está vacío. No se guarda ningún archivo.")
         return
-
-    # Calcular cuántos archivos necesitamos
-    num_archivos = math.ceil(total_rows / max_rows)
-
-    for i in range(num_archivos):
+    # Calculate how many files parts for this DataFrame
+    n_files = math.ceil(total_rows / max_rows)
+    for i in range(n_files):
         inicio = i * max_rows
         fin = inicio + max_rows
-        df_parte = df.iloc[inicio:fin]
+        df_part = df.iloc[inicio:fin]
+        file_name = f"{table}_part_{i+1}.xlsx"
+        df_part.to_excel(file_name, index=False)
 
-        nombre_archivo = f"{base_nombre}_parte_{i+1}.xlsx"
-        df_parte.to_excel(nombre_archivo, index=False)
-        print(f"Guardado: {nombre_archivo} ({len(df_parte)} filas)")
-
-    print(f"Total de {num_archivos} archivos creados.")
+    print(f"Tabla '{table}' guardada con éxito!\n")
+    return
 
 
 def execute_queries(table_list: list, queries: list) -> None:
     """
-    SQl queries are performed for every table in "table_list" and for every query in "queries"
+    SQl queries are performed for every table in "table_list" and for every query in "queries".
+    All queries are already built in "queries".
     """
     for table, query in zip(table_list, queries):
         print("Ejecutando consulta...")
         t1 = time.time()
         df = pd.read_sql(query, conn, params=receptor_RFC_list)
         print(f"Tiempo de consulta para la tabla '{table}':", time.time() - t1)
-        # split_DataFrame(df, MAX_ROWS_PER_TABLE)
-        df.to_excel(f"{table}.xlsx", index=False)
-        print(f"Tabla '{table}' guardada con éxito!\n")
+        split_DataFrame(table, df, MAX_ROWS_PER_TABLE)
+    return
 
 
 try:
